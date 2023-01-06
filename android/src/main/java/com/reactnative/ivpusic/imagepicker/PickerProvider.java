4a95bcff3c04ec7b09bdb0d669af063083a811d7
Anup481 <44887342+Anup481@users.noreply.github.com>
2023-01-06 12:24:17 +0530

    Create PickerProvider.java

---
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerProvider.java
@@ -0,0 +1,12 @@
+package com.reactnative.ivpusic.imagepicker;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * @author Anup
+ * @date 2023/01/01
+ * @email kushwaha.anup481@gmail.com
+ * @describe
+ */
+public class PickerProvider extends FileProvider {
+}
